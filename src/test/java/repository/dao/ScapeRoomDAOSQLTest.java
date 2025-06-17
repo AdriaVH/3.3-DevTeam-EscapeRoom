8bedb98576ef194b78a0bab37d40faf5a79dd653
ToniR90 <romy90.t@gmail.com>
2025-06-17 22:32:06 +0200

    ScapeRoom findAll test implement. Using Mockito

--- src/test/java/repository/dao/ScapeRoomDAOSQLTest.java
+++ src/test/java/repository/dao/ScapeRoomDAOSQLTest.java
@@ -2,25 +2,54 @@ package repository.dao;
 
 import model.ScapeRoom;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 class ScapeRoomDAOSQLTest {
 
     @Test
     void findByIdMocked() {
-            ScapeRoomDAO mockDao = Mockito.mock(ScapeRoomDAO.class);
-            ScapeRoom testRoom = new ScapeRoom(1, "Test Room" , BigDecimal.valueOf(15));
+        ScapeRoomDAO mockDao = mock(ScapeRoomDAO.class);
+        ScapeRoom testRoom = new ScapeRoom(1, "Test Room", BigDecimal.valueOf(15));
 
-            Mockito.when(mockDao.findById(1)).thenReturn(testRoom);
+        when(mockDao.findById(1)).thenReturn(testRoom);
 
-            ScapeRoom result = mockDao.findById(1);
-            assertNotNull(result);
-            assertEquals("Test Room" , result.getName());
-            assertEquals(BigDecimal.valueOf(15) , result.getTicketPrice());
+        ScapeRoom result = mockDao.findById(1);
+        assertNotNull(result);
+        assertEquals("Test Room", result.getName());
+        assertEquals(BigDecimal.valueOf(15), result.getTicketPrice());
 
-            Mockito.verify(mockDao).findById(1);
-        }
+        verify(mockDao).findById(1);
     }
+
+    @Test
+    void findAllMocked() {
+        ScapeRoomDAO mockDao = mock(ScapeRoomDAO.class);
+
+        List<ScapeRoom> testRooms = Arrays.asList(
+                new ScapeRoom(1, "Scape Room Test1", BigDecimal.valueOf(15)),
+                new ScapeRoom(2, "Scape Room Test2", BigDecimal.valueOf(8))
+        );
+
+        when(mockDao.findAll()).thenReturn(testRooms);
+
+        List<ScapeRoom> result = mockDao.findAll();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+
+        assertEquals("Scape Room Test1", result.get(0).getName());
+        assertEquals(BigDecimal.valueOf(15), result.get(0).getTicketPrice());
+        assertEquals("Scape Room Test2", result.get(1).getName());
+        assertEquals(BigDecimal.valueOf(8), result.get(1).getTicketPrice());
+
+        verify(mockDao).findAll();
+    }
+}
